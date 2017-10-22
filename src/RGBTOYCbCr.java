a668162122f049c1a29b318009a520cf16eda3f2
lhrotk <lhrotk@gmail.com>
2017-10-22 07:09:17 -0700

    RGB to ycbcr

---
+++ src/RGBTOYCbCr.java
@@ -0,0 +1,93 @@
+
+public class RGBTOYCbCr {
+	private double R;
+	private double G;
+	private double B;
+	private double Y;
+	private double Cb;
+	private double Cr;
+	
+	public RGBTOYCbCr(double R, double G, double B) {
+		this.R = R;
+		this.G = G;
+		this.B = B;
+	}
+
+	public double getR() {
+		return R;
+	}
+
+
+
+	public void setR(double r) {
+		R = r;
+	}
+
+
+
+	public double getG() {
+		return G;
+	}
+
+
+
+	public void setG(double g) {
+		G = g;
+	}
+
+
+
+	public double getB() {
+		return B;
+	}
+
+
+
+	public void setB(double b) {
+		B = b;
+	}
+
+
+
+	public double getY() {
+		return Y;
+	}
+
+
+
+	public void setY(double y) {
+		Y = y;
+	}
+
+
+
+	public double getCb() {
+		return Cb;
+	}
+
+
+
+	public void setCb(double cb) {
+		Cb = cb;
+	}
+
+
+
+	public double getCr() {
+		return Cr;
+	}
+
+
+
+	public void setCr(double cr) {
+		Cr = cr;
+	}
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
